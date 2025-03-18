0b2faab7bb363b5d7c3c77026654b66d71666602
arbic <teo.maybebaby@gmail.com>
2025-03-18 17:37:38 +0500

    inspector filter fixed

--- src/components/admin-dashboard-layout/AdminDashboardLayout.tsx
+++ src/components/admin-dashboard-layout/AdminDashboardLayout.tsx
@@ -10,7 +10,7 @@ export default function AdminDashboardLayout({
 	return (
 		<div className='bg-[#CDC1FF] dark:bg-bg text-bg dark:text-white grid min-h-screen 2xl:grid-cols-[1.1fr_6fr] grid-cols-[1.2fr-6fr] md:grid-cols-[1fr_5fr] sm:grid-col-1'>
 			<Sidebar />
-			<main className='p-big-layout overflow-x-hidden max-h-screen relative gap-4 mt-5'>
+			<main className='p-big-layout overflow-x-hidden max-h-screen relative gap-4 mt-5 w-full'>
 				<Header />
 				{children}
 				<Footer />
--- src/components/admin-dashboard-layout/header/Header.tsx
+++ src/components/admin-dashboard-layout/header/Header.tsx
@@ -40,18 +40,18 @@ export function Header() {
 
 	if (isMobile) {
 		return (
-			<header className='fixed top-0  flex justify-between p-5 rounded-xl dark:bg-sidebar items-center bg-[#A294F9] z-50 pt-[env(safe-area-inset-top,0)]'>
+			<header className='sticky top-1  flex justify-between p-5 rounded-xl dark:bg-sidebar items-center bg-[#A294F9] z-50 pt-[env(safe-area-inset-top,0)]'>
 				<Heading title={getTitle()} />
 				<div className='flex items-center gap-5 justify-between'>
-					<div className='flex items-center gap-3 dark:bg-[#2e374a] bg-[#CDC1FF] md:p-3 p-1 rounded-xl md:hidden'>
+					<div className='flex items-center gap-3 dark:bg-[#2e374a] bg-[#CDC1FF] md:p-3 w-full mt-2 rounded-xl md:hidden'>
 						<Profile />
 					</div>
-					<div className='flex gap-5 items-center'>
-						<MessageSquareMore size={20} />
-						<Bell size={20} />
-						<LanguageSelector />
-						<DarkModeSelector />
-					</div>
+				</div>
+				<div className='flex gap-5 items-center'>
+					<MessageSquareMore size={20} />
+					<Bell size={20} />
+					<LanguageSelector />
+					<DarkModeSelector />
 				</div>
 			</header>
 		)
